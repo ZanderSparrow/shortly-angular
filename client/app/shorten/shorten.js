90334fa8021a113dd8f7a9b344ac12428e6992f9
Silverhawk <sondra.silverhawk@hush.com>
2016-02-13 10:57:11 -0800

    Add style and a spinner

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,9 +3,11 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
   $scope.data = {};
+  $scope.loading = false;
 
   $scope.addLink = function () {
     $scope.link.url = $scope.newLink;
+    $scope.loading = true;
     Links.addOne($scope.link)
     .then(function (res) {
       $scope.data.link = res.data;
